0238137df2f9a1a887f9d1f1c0468ac3fd9c5257
ninehusky <acheung8@uw.edu>
2019-11-09 13:37:59 -0800

    More opcodes, changed erroneous programCounter pointer to correct opcode pointer

--- Chip8.java
+++ Chip8.java
@@ -163,8 +163,8 @@ public class Chip8 {
      * the top of the stack, then subtracts 1 from the stack pointer.
      */
     private void opcode00EE() {
-        programCounter = stackPointer;
         stackPointer--;
+        programCounter = stackPointer;
     }
 
     /**
@@ -172,7 +172,7 @@ public class Chip8 {
      * Jump to location nnn.
      */
     private void opcode1nnn() {
-        char address = (char)(programCounter & 0x0FFF);
+        char address = (char)(opcode & 0x0FFF);
         programCounter = address;
     }
 
@@ -196,7 +196,7 @@ public class Chip8 {
      * increments the program counter by 2.
      */
     private void opcode3xkk() {
-        char x = (char)((programCounter & 0x0F00) >>> 2);
+        char x = (char)((programCounter & 0x0F00) >> 8);
         char kk = (char)(programCounter & 0x00FF);
         if (registers[x] == kk) {
             programCounter += 2;
@@ -211,8 +211,8 @@ public class Chip8 {
      * increments program counter by 2.
      */
     private void opcode4xkk() {
-        char x = (char)((programCounter & 0x0F00) >>> 2);
-        char kk = (char)(programCounter & 0x00FF);
+        char x = (char)((opcode & 0x0F00) >> 8);
+        char kk = (char)(opcode & 0x00FF);
         if (registers[x] != kk) {
             programCounter += 2;
         }
@@ -225,8 +225,8 @@ public class Chip8 {
      * are equal increments program counter by 2.
      */
     private void opcode5xy0() {
-        char x = (char)((programCounter & 0x0F00) >>> 2);
-        char y = (char)((programCounter & 0x00F0) >>> 1);
+        char x = (char)((opcode & 0x0F00) >> 8);
+        char y = (char)((opcode & 0x00F0) >> 4);
         if (registers[x] == registers[y]) {
             programCounter += 2;
         }
@@ -239,21 +239,159 @@ public class Chip8 {
      */
     private void opcode6xkk() {
         byte kk = (byte)(programCounter & 0x00FF);
-        char x = (char)((programCounter & 0x0F00) >>> 2);
+        char x = (char)((programCounter & 0x0F00) >> 8);
         // TODO: i don't think I need to use a char, since 0 <= x <= 16
         registers[x] = kk;
     }
 
     /**
+     * ADD Vx, byte
      * Set Vx = Vx + kk.
      * Adds the value kk to the value of register Vx,
      * then stores the result in Vx.
      */
     private void opcode7xkk() {
-        byte kk = (byte)(programCounter & 0x00FF);
-        char x = (char)((programCounter & 0x0F00) >>> 2);
+        byte kk = (byte)(opcode & 0x00FF);
+        byte x = (byte)((opcode & 0x0F00) >> 8);
         registers[x] += kk;
     }
 
+    /**
+     * LD Vx, Vy
+     * Set Vx = Vy
+     * Stores the value of register Vy into register Vx.
+     */
+    private void opcode8xy0() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        registers[x] = registers[y];
+    }
+
+    /**
+     * OR Vx, Vy
+     * Set Vx = Vx OR Vy
+     * Performs a bitwise OR on the values of Vx and Vy, then stores
+     * the result in Vx.
+     */
+    private void opcode8xy1() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        registers[x] |= registers[y];
+    }
+
+    /**
+     * AND Vx, Vy
+     * Set Vx = Vx AND Vy
+     */
+    private void opcode8xy2() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        registers[x] &= registers[y];
+    }
+
+    /**
+     * XOR Vx, Vy
+     * Set Vx = Vx XOR Vy
+     */
+    private void opcode8xy3() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        registers[x] ^= registers[y];
+    }
+
+    /**
+     * ADD Vx, Vy
+     * Set Vx = Vx + Vy, Set VF = carry
+     * The values of Vx and Vy are added together.
+     * If the result is greater than 8 bits (i.e., > 255,) VF is set to 1,
+     * (VF --> registers[15])
+     * otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+     */
+    private void opcode8xy4() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        int sum = registers[x] + registers[y];
+        if (sum > 255) {
+            registers[0xF] = 1;
+        } else {
+            registers[0xF] = 0;
+        }
+        registers[x] = (byte)sum;
+    }
+
+    /**
+     * SUB Vx, Vy
+     * Set Vx = Vx - Vy, set VF = NOT borrow.
+     * If Vx > Vy, then VF is set to 1, otherwise 0.
+     * Then Vy is subtracted from Vx, and the results stored in Vx.
+     */
+    private void opcode8xy5() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y = (byte)((opcode & 0x00F0) >> 4);
+        registers[0xF] = (byte)(registers[x] > registers[y] ? 1 : 0);
+        registers[x] -= registers[y];
+    }
+
+    /**
+     * SHR Vx {, Vy}
+     * Set Vx = Vx SHR 1
+     * If the least-significant bit of Vx is 1, then VF is set to 1,
+     * otherwise 0. Then Vx is divided by 2.
+     * In this case, least-significant bit is rightmost bit.
+     * TODO: look over this, it might not be good
+     */
+    private void opcode8xy6() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        registers[0xF] = (byte)(registers[x] & 0x1);
+        registers[x] >>= 1;
+    }
+
+    /**
+     * SUBN Vx, Vy
+     * Set Vx = Vy - Vx, set VF = NOT borrow
+     * If Vy > Vx, VF Set to 1, else 0.
+     * Then Vx subtracted from Vy, results stored in Vx.
+     */
+    private void opcode8xy7() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte y =  (byte)((opcode & 0x00F0) >> 4);
+        registers[0xF] = (byte)(registers[y] > registers[x] ? 1 : 0);
+        registers[x] = (byte)(registers[y] - registers[x]);
+    }
+
+    /**
+     * SHL Vx {, Vy}
+     * Vx = Vx SHL 1
+     * If most significant bit of Vx is 1, then VF is set to 1, else 0
+     * Then Vx multiplied by 2.
+     * TODO: look over this
+     */
+    private void opcode8xy7() {
+        byte x = (byte)((opcode & 0x0F00) >> 8);
+        byte mostSignificantBit = (byte)(registers[x] >> 7);
+        registers[0xF] = (byte)(mostSignificantBit);
+        registers[x] <<= 1; // multiply Vx by 2
+    }
+
+    /**
+     * SNE Vx, Vy
+     * Skip next instruction if Vx != Vy
+     */
+    private void opcode9xy0() {
+        byte x = (byte)((programCounter & 0x0F00) >> 8);
+        byte y = (byte)((programCounter & 0x00F0) >> 4);
+        if (registers[x] != registers[x]) {
+            programCounter += 2;
+        }
+    }
+
+    /**
+     * LD I, addr
+     * Value of register I is set to nnn.
+     */
+    private void opcodeAnnn() {
+        char value = (char)(programCounter & 0x0FFF);
+        indexRegister = value;
+    }
 
 }
